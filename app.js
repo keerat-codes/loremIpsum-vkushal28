a0665beefebdfc361e5fd6086b1fe44aac71b60b
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 21:25:39 +0530

    Updated App.js for pringles route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const pringlesRoute = require('./routes/pringles');
+app.use('/pringles', pringlesRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
