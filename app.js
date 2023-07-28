be1749183f29c7ea752a911647a879475d150b97
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-28 23:43:35 +0530

    Updated app.js using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 const pringlesRoute = require('./routes/pringles');
 const makePringles  = require('./controllers/makePringles');
 const banana chipsRoute = require('./routes/banana chips');
+const test  = require('./controllers/test');
+app.use('/asfas', test);
 app.use('/banana chips', banana chipsRoute);
 app.use('/pringles/sourCream', makePringles);
 app.use('/pringles', pringlesRoute);
